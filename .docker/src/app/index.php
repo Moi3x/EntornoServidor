702820c76e867534761e4c261f6bf42f226c990b
Moi3x <mgarciag4@alu.cipfpvalledeelda.com>
2026-09-21 20:49:21 +0200

    ejemplos de codigo

--- .docker/src/app/index.php
+++ .docker/src/app/index.php
@@ -1,3 +1,3 @@
 <?php
 //include "app/view/frontend/index.html";
-include "app/view/backend/index.html";
+include ".docker/src/app/view/backend/index.html";
--- .docker/src/index.php
+++ .docker/src/index.php
@@ -1,4 +1,4 @@
 <?php
 //include "app/view/frontend/index.html";
 // /barra/admin vas al backend
-include "app/view/backend/index.html";
+include ".docker/src/app/view/backend/index.html";
