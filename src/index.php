c1bd3ceae197a8b2bc44af4d0c671e5bad87347c
Moi3x <mgarciag4@alu.cipfpvalledeelda.com>
2026-09-24 19:49:37 +0200

    ejemplos de codigo

--- src/index.php
+++ src/index.php
@@ -4,13 +4,20 @@
 
 include_once "./vendor/autoload.php";
 use Phroute\Phroute\RouteCollector;
+use Phroute\Phroute\Dispatcher;
 $router = new RouteCollector();
 $router->get('/admin', function() {
     return 'Estás intentando acceder a la función admin';
 });
+$router->get('/', function() {
+    return 'Página principal';
+});
+
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
 
-$response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$response = $dispatcher->dispatch(
+    $_SERVER['REQUEST_METHOD'],
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 // Print out the value returned from the dispatched function
 echo $response;
